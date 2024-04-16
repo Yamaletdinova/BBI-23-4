50baa41af3558d5f983292fd7ef70ef40b1e3e1c
Yamaletdinova <5m_alsu@mail.ru>
2024-04-16 21:10:04 +0300

    ЛР6_2_защита

---
+++ ЛР6_2_защита/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР6_2_защита
+{
+    struct Sportsmen
+    {
+        private string _surname;
+        private double _bestResult;
+        public Sportsmen(string surname, double rez1, double rez2, double rez3) 
+        {
+
+            _surname = surname;
+            _bestResult = 0;
+
+            double maxx = 0;
+            double[] tec = new double[3] { rez1, rez2, rez3 };
+            for (int i = 0; i < 3; i++)
+            {
+                if (tec[i] > maxx)
+                {
+                    maxx = tec[i];
+                }
+            }
+            _bestResult = maxx;
+
+        }
+        public double BestResult 
+        {
+            get => _bestResult;
+        }
+
+        public void Print() => Console.WriteLine("Фамилия: {0,10} Результат: {1,10}", _surname, _bestResult); 
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Sportsmen[] players = new Sportsmen[5];
+                players[0] = new Sportsmen("Иванов", 5, 4, 3);
+                players[1] = new Sportsmen("Лебедев", 6, 4, 7);
+                players[2] = new Sportsmen("Андреев", 9, 8, 9);
+                double[] end = new double[3];
+
+                for (int i = 0; i < 3; i++)
+                {
+                    players[i].Print();
+                }
+            }
+
+        }
+    }
+}
---
+++ ЛР6_2_защита/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ЛР6_2_защита")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ЛР6_2_защита")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("312c4ad0-6937-4a40-b0a8-794dc684a334")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
